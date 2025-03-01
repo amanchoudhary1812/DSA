11b0f0e15149dc3a2b77f8c7f240ca6b9bc72b8e
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-01 18:47:36 +0530

    Commit

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class RemoveElement {
-    public static int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) 
+    {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {

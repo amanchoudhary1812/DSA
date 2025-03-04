006386133416767b7fa836626767a0751c613937
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-04 17:11:57 +0530

    Commit

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -5,7 +5,7 @@ public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;     // Index for the last element in nums1's initial segment
         int j = n - 1;     // Index for the last element in nums2
-        int k = m + n - 1; // Index for the last position in nums1
+        int k = m + n - 1; // Index for the last position in  nums1
 
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j]) {

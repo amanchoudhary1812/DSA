c0364e5f770eb17adcd2cdb722eed11bd109e900
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-05 16:57:08 +0530

    Commit

---
+++ SortedArrayToBST.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class SortedArrayToBST {
+    
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildBST(nums, left, mid - 1);
+        node.right = buildBST(nums, mid + 1, right);
+        return node;
+    }
+
+    // Function to print the tree in Preorder Traversal
+    public static void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the number of elements in the sorted array:");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        
+        System.out.println("Enter the sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        scanner.close();
+        
+        TreeNode root = sortedArrayToBST(nums);
+        
+        System.out.println("Preorder traversal of the constructed BST:");
+        preOrder(root);
+    }
+}

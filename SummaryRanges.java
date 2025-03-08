47bc4f13cb32eb722b834bf98a0b9ed1630b11ba
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-08 17:25:34 +0530

    Commit

--- SummaryRanges.java
+++ SummaryRanges.java
@@ -12,8 +12,7 @@ public class SummaryRanges {
         int start = 0;
         while (start < nums.length) {
             int end = start;
-            while (end + 1 < nums.length && nums[end + 1] == nums[end] + 1) 
-            {
+            while (end + 1 < nums.length && nums[end + 1] == nums[end] + 1) {
                 end++;
             }
             if (end == start) {

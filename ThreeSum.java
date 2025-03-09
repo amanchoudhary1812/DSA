742aec173a020fd1fd9e65c7af0c879f576fe9d0
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-09 18:56:23 +0530

    Commit

--- ThreeSum.java
+++ ThreeSum.java
@@ -22,7 +22,8 @@ public class ThreeSum {
                     right--;
                 } else if (sum < 0) {
                     left++;
-                } else {
+                } else 
+                {
                     right--;
                 }
             }

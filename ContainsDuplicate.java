17302a58f11bdc04620019cc22d9532fb6b65171
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-10 22:23:40 +0530

    Commit

---
+++ ContainsDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true; // Duplicate found
+            }
+        }
+        return false; // No duplicates
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        scanner.close();
+        
+        System.out.println("Contains Duplicate: " + containsDuplicate(nums));
+    }
+}

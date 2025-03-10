ca5f5018b13afc992985c1c6c8bf5ff7fe5c8d56
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-10 22:24:22 +0530

    Commit

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -19,7 +19,8 @@ public class ContainsDuplicate {
         int n = scanner.nextInt();
         int[] nums = new int[n];
         System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             nums[i] = scanner.nextInt();
         }
         scanner.close();

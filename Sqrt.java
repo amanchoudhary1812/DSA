e1332b0b3325c4d0826ebed06a045ffd689732a9
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-13 18:52:14 +0530

    Commit

--- Sqrt.java
+++ Sqrt.java
@@ -26,7 +26,9 @@ public class Sqrt {
     }
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a non-negative integer: ");
         int x = scanner.nextInt();
         scanner.close();

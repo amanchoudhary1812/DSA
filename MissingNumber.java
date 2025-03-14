a11b8c7aa61e3216483c445237a2edfa47327f13
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-14 22:43:34 +0530

    Commit

---
+++ MissingNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MissingNumber {
+    public static int findMissingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        scanner.close();
+        System.out.println("Missing number: " + findMissingNumber(nums));
+    }
+}

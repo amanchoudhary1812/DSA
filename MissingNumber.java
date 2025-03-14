35c58de3c355dd55099bcdf3f68bb8841380a6ab
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-14 22:44:22 +0530

    Commit

--- MissingNumber.java
+++ MissingNumber.java
@@ -14,8 +14,9 @@ public class MissingNumber {
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter the size of the array: ");
+        
         int n = scanner.nextInt();
         int[] nums = new int[n];
         System.out.println("Enter " + n + " elements:");

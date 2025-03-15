884b0b2ca435f68441548ff32d0183ab967e3f2c
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-15 18:28:44 +0530

    Commit

--- SingleNumber.java
+++ SingleNumber.java
@@ -18,7 +18,8 @@ public class SingleNumber {
         int n = scanner.nextInt();
         int[] nums = new int[n];
         System.out.println("Enter the elements: ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             nums[i] = scanner.nextInt();
         }
         scanner.close();

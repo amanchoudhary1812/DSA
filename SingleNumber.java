9caf0de985572da639b1bad6d8432597a838c84b
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-15 18:28:18 +0530

    Commit

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class SingleNumber {
-    public static int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) 
+    {
         int result = 0;
         for (int num : nums) {
             result ^= num;

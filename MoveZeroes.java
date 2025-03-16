483b9341bca51c35f0e3a87fa0779155b1ad5ca7
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-16 18:57:21 +0530

    Commit

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -17,7 +17,8 @@ public class MoveZeroes {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int[] nums = {0, 1, 0, 3, 12};
         System.out.println("Before: " + Arrays.toString(nums));
         moveZeroes(nums);

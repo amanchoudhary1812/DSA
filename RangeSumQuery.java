e5eaa80284ca2caa2060c1f4378a34f204568ed5
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-17 12:35:29 +0530

    Commit

--- RangeSumQuery.java
+++ RangeSumQuery.java
@@ -5,7 +5,8 @@ public class RangeSumQuery {
 
     public RangeSumQuery(int[] nums) {
         prefix = new int[nums.length + 1];
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; ++i) 
+        {
             prefix[i + 1] = nums[i] + prefix[i];
         }
     }

bc11754d0518f11ca1bb43edfcf0e48df56ba1ef
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-18 15:17:14 +0530

    Commit

--- DuplicateNumber.java
+++ DuplicateNumber.java
@@ -25,7 +25,8 @@ public class DuplicateNumber
         return slow;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();

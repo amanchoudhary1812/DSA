f1d6882dc1384baea161a630b409b8400b2046a1
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-21 18:53:18 +0530

    Commit

--- LargestNumber.java
+++ LargestNumber.java
@@ -39,7 +39,8 @@ public class LargestNumber
         return largestNumber.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();

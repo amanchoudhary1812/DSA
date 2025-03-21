5d89bc8ed47c23ebfcfae5aa041c8fdae601843a
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-03-21 18:52:19 +0530

    Commit

--- LargestNumber.java
+++ LargestNumber.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class LargestNumber {
+public class LargestNumber 
+{
     public static String largestNumber(int[] nums) {
         // Convert integers to strings
         String[] strNums = Arrays.stream(nums)

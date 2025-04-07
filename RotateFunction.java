6e8a2299283febfcf518f8f2b8dc7ee810b7bf18
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-04-07 22:32:02 +0530

    Commit

---
+++ RotateFunction.java
@@ -0,0 +1,30 @@
+public class RotateFunction {
+    public static int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int F = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += i * nums[i];
+        }
+
+        int max = F;
+
+        for (int k = 1; k < n; k++) {
+            F = F + sum - n * nums[n - k];
+            max = Math.max(max, F);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // Sample input
+        int[] nums = {4, 3, 2, 6};
+
+        // Function call and output
+        int result = maxRotateFunction(nums);
+        System.out.println("Maximum Rotate Function value: " + result);
+    }
+}

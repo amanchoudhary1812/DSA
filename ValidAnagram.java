78b35649215aeca4cca0ff28f85beb978adf4f20
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-05-04 20:04:57 +0530

    Commit

---
+++ ValidAnagram.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s = scanner.nextLine().toLowerCase();
+        System.out.print("Enter second string: ");
+        String t = scanner.nextLine().toLowerCase();
+
+        boolean result = isAnagram(s, t);
+        if (result) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+
+        scanner.close();
+    }
+}

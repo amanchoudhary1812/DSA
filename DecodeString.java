f7b6b047a12c2b80ba8a7291081b51bdcdcf2248
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-05-17 22:36:16 +0530

    Commit

---
+++ DecodeString.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class DecodeString {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                int currentK = countStack.pop();
+                for (int i = 0; i < currentK; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+
+        return currentString.toString();
+    }
+
+    public static void main(String[] args) {
+        DecodeString decoder = new DecodeString();
+        String input = "3[a2[c]]";
+        String output = decoder.decodeString(input);
+        System.out.println("Decoded String: " + output);
+    }
+}

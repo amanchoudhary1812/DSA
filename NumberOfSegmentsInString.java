08d1e252f1f56ab2acf87606d0f3b92e9ba5dccf
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-05-29 09:59:59 +0530

    Commit

---
+++ NumberOfSegmentsInString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NumberOfSegmentsInString {
+
+    public static int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        int segments = countSegments(input);
+        System.out.println("Number of segments: " + segments);
+        scanner.close();
+    }
+}

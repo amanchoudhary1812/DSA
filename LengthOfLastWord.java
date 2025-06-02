1e21f74442ea852e8ca341bc8855fb88604db711
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-06-02 19:24:10 +0530

    Commit

---
+++ LengthOfLastWord.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        int result = lengthOfLastWord(input);
+        System.out.println("Length of last word: " + result);
+    }
+}

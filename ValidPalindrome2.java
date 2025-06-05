6e8c8fb810dbb0d1df3a4f8bb7f0ad6c155608a1
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-06-05 15:49:12 +0530

    Commit

---
+++ ValidPalindrome2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ValidPalindrome2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        boolean result = validPalindrome(input);
+        System.out.println("Can the string be a valid palindrome after at most one deletion? " + result);
+    }
+
+    public static boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+} 

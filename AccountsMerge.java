a4b6b6a055659f65e96af352cc06135dc8116ce7
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-06-30 20:02:54 +0530

    Commit

--- AccountsMerge.java
+++ AccountsMerge.java
@@ -56,8 +56,9 @@ public class AccountsMerge {
     public static void main(String[] args) {
         List<List<String>> accounts = new ArrayList<>();
         accounts.add(Arrays.asList("Aman", "johnsmith@mail.com", "john00@mail.com"));
-        
+
         accounts.add(Arrays.asList("John", "johnnybravo@mail.com"));
+        
         accounts.add(Arrays.asList("John", "johnsmith@mail.com", "john_newyork@mail.com"));
         accounts.add(Arrays.asList("Mary", "mary@mail.com"));
 

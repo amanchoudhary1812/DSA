1f1183968824a15317efaaec576fe5bccd37e513
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-07-06 22:54:43 +0530

    Commit

--- ConsecutiveCharacters.java
+++ ConsecutiveCharacters.java
@@ -19,7 +19,8 @@ public class ConsecutiveCharacters {
         return maxCount;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();

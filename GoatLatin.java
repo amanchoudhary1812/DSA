abf4cb84b703ae13a3941fbd53cddb31560b7847
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-07-11 23:18:57 +0530

    Commit

--- GoatLatin.java
+++ GoatLatin.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class GoatLatin {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a sentence:");
         String sentence = scanner.nextLine();

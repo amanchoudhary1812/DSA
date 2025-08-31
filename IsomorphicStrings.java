a0116c143043369b2ad27c46afc7b295532c0cc0
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-31 21:08:32 +0530

    Commit

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -32,7 +32,6 @@ public class IsomorphicStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first string: ");
-        
         String s = scanner.nextLine();
 
         System.out.print("Enter second string: ");

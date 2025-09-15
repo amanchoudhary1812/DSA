d456423998f739f30b8eefbf1b53c96ddbe17fd0
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-09-15 23:42:07 +0530

    Commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -12,8 +12,7 @@ public class FizzBuzz {
             if (i % 3 == 0) sb.append("Fizz");
             if (i % 5 == 0) sb.append("Buzz");
 
-            if (sb.length() == 0) 
-            {
+            if (sb.length() == 0) {
                 sb.append(i);
             }
 

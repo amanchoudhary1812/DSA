8485440c223dccd5c0d3ad4552fbcec4b7bc6f0d
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-09-27 20:06:24 +0530

    Commit

--- MiniParser.java
+++ MiniParser.java
@@ -115,7 +115,8 @@ class Solution {
 }
 
 public class MiniParser {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter nested integer string: ");
         String input = sc.nextLine();

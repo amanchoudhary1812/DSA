a3b90955c0092c7fe7374b024da985b94af4ecf9
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-10-03 21:43:12 +0530

    Commit

--- JumpGame.java
+++ JumpGame.java
@@ -5,7 +5,7 @@ public class JumpGame {
         int reachable = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i > reachable) {
-                return false; // Can't reach this index
+                return false; 
             }
             reachable = Math.max(reachable, i + nums[i]);
             if (reachable >= nums.length - 1) 

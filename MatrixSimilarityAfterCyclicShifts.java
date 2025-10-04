1595bc3c755b25464e7a7b9d435a69607ee377db
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-10-04 21:51:06 +0530

    Commit

--- MatrixSimilarityAfterCyclicShifts.java
+++ MatrixSimilarityAfterCyclicShifts.java
@@ -4,7 +4,6 @@ public class MatrixSimilarityAfterCyclicShifts {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 
-```
     // Input rows and cols
     System.out.print("Enter number of rows: ");
     int m = sc.nextInt();

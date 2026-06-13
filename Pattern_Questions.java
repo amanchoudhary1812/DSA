821c07929e5c5c3f10eca4f5cb1e1766d23a464f
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-13 17:08:12 +0530

    Commit

--- Pattern_Questions.java
+++ Pattern_Questions.java
@@ -1,9 +1,10 @@
 public class Pattern_Questions {
     public static void main (String[] args) {
-        pattern1(4);
-        pattern2(5);
-        pattern3(5);
-        pattern4(5);
+        // pattern1(4);
+        // pattern2(5);
+        // pattern3(5);
+        // pattern4(5);
+        pattern5(5);
     }
 
     static void pattern1 (int n) {
@@ -44,6 +45,16 @@ public class Pattern_Questions {
             System.out.println();
         }
         System.out.println();
+    }
+
+    static void pattern5 (int n) {
+        for (int row = 0; row <= 2*n; row++) {
+            int totalColumnsInRow = row > n ? 2*n - row : row;
+            for (int column = 0; column < totalColumnsInRow; column++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
 

c24d6c7716f2473c55b762fbf255d02809453d32
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-15 11:56:45 -0700

    Commit

--- Pattern_Questions.java
+++ Pattern_Questions.java
@@ -4,8 +4,8 @@ public class Pattern_Questions {
         // pattern2(5);
         // pattern3(5);
         // pattern4(5);
-        // pattern5(5);
-         pattern6(5);
+        pattern5(5);
+        pattern6(5);
         pattern7(5);
         pattern8(5);
 }

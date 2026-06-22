73abf3147b53ea5fb12483088aedc7334ed9351f
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-22 23:40:36 +0530

    Commit

---
+++ SecondLargest.java
@@ -0,0 +1,18 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 1, 8, 7, 4, 9};
+
+        int FL = Integer.MIN_VALUE;
+        int SL = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > FL) {
+                SL = FL;
+                FL = arr[i];
+            } else if (arr[i] > SL && arr[i] != FL) {
+                SL = arr[i];
+            }
+        }
+        System.out.println(SL);
+    }
+}

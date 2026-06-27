3674050d5b0145fa2bb3c788e8a6dd210d994849
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-27 23:29:57 +0530

    Commit (Leetcode solution)

--- Valid_Parentheses.java
+++ Valid_Parentheses.java
@@ -47,3 +47,33 @@ public class Valid_Parentheses {
         sc.close();
     }
 }
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                
+            }
+        }
+        return stack.isEmpty();
+    }
+}

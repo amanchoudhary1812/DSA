efafdc01a7eddcd236396087c42d7adb2833da0a
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-30 22:25:07 +0530

    Commit

---  AverageOfLevelsInBinaryTree.java
+++  AverageOfLevelsInBinaryTree.java
@@ -52,14 +52,14 @@ public class AverageOfLevelsInBinaryTree {
         return result;
     }
 
-    // public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    //     TreeNode root = new TreeNode(3);
-    //     root.left = new TreeNode(9);
-    //     root.right = new TreeNode(20);
-    //     root.right.left = new TreeNode(15);
-    //     root.right.right = new TreeNode(7);
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
 
-    //     System.out.println(averageOfLevels(root));
-    // }
+        System.out.println(averageOfLevels(root));
+    }
 }
